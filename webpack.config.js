d879fd45d99d3e61d30b7705d9fe2dc5f5b5b503
Sumanth Madishetty <sumanth.m@techsophy.com>
2020-05-01 16:28:57 +0530

    webpack with playground

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const pkg = require("./package.json");
 const path = require("path");
 const libraryName = pkg.name;
+
 module.exports = {
   entry: path.join(__dirname, "./src/index.js"),
   output: {
@@ -11,13 +12,13 @@ module.exports = {
     library: libraryName,
     libraryTarget: "commonjs2",
     publicPath: "/dist/",
-    umdNamedDefine: true
+    umdNamedDefine: true,
   },
   plugins: [],
   node: {
     net: "empty",
     tls: "empty",
-    dns: "empty"
+    dns: "empty",
   },
   module: {
     rules: [
@@ -30,40 +31,40 @@ module.exports = {
               fallback: "file-loader",
               name: "[name][md5:hash].[ext]",
               outputPath: "assets/",
-              publicPath: "/assets/"
-            }
-          }
-        ]
+              publicPath: "/assets/",
+            },
+          },
+        ],
       },
       {
         test: /\.*css$/,
         use: ExtractTextPlugin.extract({
           fallback: "style-loader",
-          use: ["css-loader", "sass-loader"]
-        })
+          use: ["css-loader", "sass-loader"],
+        }),
       },
       {
         test: /\.(js|jsx)$/,
         use: ["babel-loader"],
         include: path.resolve(__dirname, "src"),
-        exclude: /node_modules/
+        exclude: /node_modules/,
       },
       {
         test: /\.(eot|ttf|woff|woff2)$/,
-        use: ["file-loader"]
+        use: ["file-loader"],
       },
       {
         test: /\.(pdf|doc|zip)$/,
-        use: ["file-loader"]
-      }
-    ]
+        use: ["file-loader"],
+      },
+    ],
   },
   resolve: {
     alias: {
       react: path.resolve(__dirname, "./node_modules/react"),
       "react-dom": path.resolve(__dirname, "./node_modules/react-dom"),
-      assets: path.resolve(__dirname, "assets")
-    }
+      assets: path.resolve(__dirname, "assets"),
+    },
   },
   externals: {
     // Don't bundle react or react-dom
@@ -71,13 +72,13 @@ module.exports = {
       commonjs: "react",
       commonjs2: "react",
       amd: "React",
-      root: "React"
+      root: "React",
     },
     "react-dom": {
       commonjs: "react-dom",
       commonjs2: "react-dom",
       amd: "ReactDOM",
-      root: "ReactDOM"
-    }
-  }
+      root: "ReactDOM",
+    },
+  },
 };
